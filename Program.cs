785abf7162b2967b7a41b394ec56da4cc0bad923
S1CAY <artem.sklyarskiy@gmail.com>
2023-10-04 09:58:18 +0300

    commit lab1

--- Program.cs
+++ Program.cs
@@ -1,71 +1,29 @@
 ﻿using System;
-
-class Address
+namespace lab1_2023
 {
-    private string index;
-    private string country;
-    private string city;
-    private string street;
-    private string house;
-    private string apartment;
+    class Program
 
-    public string Index
     {
-        get { return index; }
-        set { index = value; }
+        static void Main(string[] args)
+        {
+            Address myAddress = new Address();
+            myAddress.Index = "12345";
+            myAddress.Country = "USA";
+            myAddress.City = "New York";
+            myAddress.Street = "Broadway";
+            myAddress.House = "123";
+            myAddress.Apartment = "456";
+
+            Console.WriteLine("Поштова адреса:");
+            Console.WriteLine($"Індекс: {myAddress.Index}");
+            Console.WriteLine($"Країна: {myAddress.Country}");
+            Console.WriteLine($"Місто: {myAddress.City}");
+            Console.WriteLine($"Вулиця: {myAddress.Street}");
+            Console.WriteLine($"Будинок: {myAddress.House}");
+            Console.WriteLine($"Квартира: {myAddress.Apartment}");
+
+            Console.ReadLine();
+        }
     }
 
-    public string Country
-    {
-        get { return country; }
-        set { country = value; }
-    }
-
-    public string City
-    {
-        get { return city; }
-        set { city = value; }
-    }
-
-    public string Street
-    {
-        get { return street; }
-        set { street = value; }
-    }
-
-    public string House
-    {
-        get { return house; }
-        set { house = value; }
-    }
-
-    public string Apartment
-    {
-        get { return apartment; }
-        set { apartment = value; }
-    }
-}
-
-class Program
-{
-    static void Main(string[] args)
-    {
-        Address myAddress = new Address();
-        myAddress.Index = "12345";
-        myAddress.Country = "USA";
-        myAddress.City = "New York";
-        myAddress.Street = "Broadway";
-        myAddress.House = "123";
-        myAddress.Apartment = "456";
-
-        Console.WriteLine("Поштова адреса:");
-        Console.WriteLine($"Індекс: {myAddress.Index}");
-        Console.WriteLine($"Країна: {myAddress.Country}");
-        Console.WriteLine($"Місто: {myAddress.City}");
-        Console.WriteLine($"Вулиця: {myAddress.Street}");
-        Console.WriteLine($"Будинок: {myAddress.House}");
-        Console.WriteLine($"Квартира: {myAddress.Apartment}");
-
-        Console.ReadLine();
-    }
 }
